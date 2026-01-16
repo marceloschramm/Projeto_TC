380a9808166e41a6962ce2458bd96ab13d4695cf
marceloschramm <celoschramm@gmail.com>
2026-01-16 08:44:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -55,19 +55,19 @@ st.write('Navegue entre as abas abaixo. Em caso de dúvida acesse a aba Info')
 # DEFININDO AS OPÇÕES PARA VISUALIZAÇÃO
 #-----------------------------------
 dict_acoes = {
-    'Banco do Brasil': 'BBAS3.SA',
-    'IBOVESPA': '^BVSP'
+    'IBOVESPA': '^BVSP',
+    'Banco do Brasil': 'BBAS3.SA'
 }
 
 dict_periodos = {
-    '5 dias': "5d",
     '1 mês': "1mo",
     '3 meses': "3mo",
     '6 meses': "6mo",
     '1 ano': "1y",
     '3 anos': "3y",
     '5 anos': "5y",
-    '10 anos': "10y"
+    '10 anos': "10y",
+    '5 dias': "5d"
 }
 #-----------------------------------
 # CONFIGURANDO A COLUNA LATERAL
@@ -76,7 +76,7 @@ with st.sidebar:
     
     st.image('bola_cristal.jpg')
     
-    st.write("Selecione um período e uma ação ou índice.")
+    st.write("Selecione os períodos e uma ação ou índice.")
     
     add_selectbox_acao = st.selectbox(
     "Escolha o papel",
@@ -143,7 +143,8 @@ with open(f'app/metadata/meta_{PAPEL}.txt', "r", encoding="utf-8") as arquivo:
 
 logger.info(f'Metadados (meta_{PAPEL}.txt) lidos com sucesso')
 
-data_treino = metadados['data_treino'] #data final do DF utilizado no treino e teste do modelo
+data_treino_str = metadados['data_treino'] #data final do DF utilizado no treino e teste do modelo
+data_treino = datetime.strptime(data_treino_str, '%Y-%m-%d %H:%M:%S')
 modelo = metadados['model_name']
 data_final_str = metadados['data_teste']
 data_final = pd.to_datetime(data_final_str) #transforma a string da data lida no txt no formato datetime
@@ -152,29 +153,114 @@ papel_treino = metadados['papel']
 ticker = dict_acoes[add_selectbox_acao]
 periodo = dict_periodos[add_selectbox_periodo] #periodo apenas para a aba de visualização
 papel = Acao(ticker)
-df_vis = papel.retorna_df(periodo, data_final) #monta o df para visualização
+df_vis = papel.retorna_df(periodo, data_treino) #monta o df para visualização
 df = papel.retorna_df('10y', data_final) # df para tabelas dos modelos de predição
 
 logger.info('DataFrames carregados com sucesso')
 
+#---------------------------------------------------
+# BAIXANDO OS MODELOS E REALIZANDO PREVISÕES
+#---------------------------------------------------
+#--------------------------------------------
+# ajuste do DF para uso dos modelos
+#--------------------------------------------
 
+h=add_previsao #horizonte de previsão
+limite = df.index[-h] #timestamp que determina treino e teste
+#especificações do statsforecast
+df_stats = df[['minima', 'maxima', 'fechamento']].copy()
+df_ar = df_stats.reset_index()
+df_ar = df_ar.rename(columns={'Date': 'ds', 'fechamento': 'y'})
+df_ar['unique_id'] = 1
+#definição de treino e teste
+treino = df_ar.loc[df_ar['ds'] < limite]
+teste = df_ar.loc[df_ar['ds'] >= limite]
+logger.info('Períodos de treino e teste definidos')
+ 
+#------------------------------------------
+# PREDIÇÃO DAS EXÓGENAS: (RETIRADO NA VERSÃO 1.1)
+# Carrega os modelos já treinados, e depois junta as previsões
+# em um único DF que irá servir de dados externos para o modelo
+# principal.
+#------------------------------------------
+
+#model_min = joblib.load(f'app/models/model_prev_min_{PAPEL}.pkl')
+#logger.info('Modelo de previsão da mínima carregado')
+#exog_min = model_min.predict(h=h)
+#exog_min.rename(columns={'AutoARIMA': 'minima'}, inplace=True)
+#model_max = joblib.load(f'app/models/model_prev_max_{PAPEL}.pkl')
+#logger.info('Modelo de previsão da máxima carregado')
+#exog_max = model_max.predict(h=h)
+#exog_max.rename(columns={'AutoARIMA': 'maxima'}, inplace=True)
+#exog = exog_min.merge(exog_max[['ds', 'maxima']], on='ds', how='left')
+#logger.info('DF de máxima e mínima criado')
+
+#-------------------------------------------------------------
+# LOAD DO MODELO TREINADO E PREDIÇÃO DO PERÍODO SELECIONADO
+#-------------------------------------------------------------
+#@st.cache_resource
+def load_modelos():
+    scaler = joblib.load(f'app/models/scaler_{PAPEL}.pkl')
+    modelo = joblib.load(f'app/models/model_{PAPEL}.pkl')
+    return scaler, modelo
+
+scaler, modelo_ar = load_modelos()
+#modelo_ar = joblib.load(f'app/models/model_{PAPEL}.pkl')
+logger.info('Modelo de previsão principal carregado')
+df_pred = modelo_ar.predict(h=h)#, X_df=exog)
+df_pred['ARIMA'] = scaler.inverse_transform(df_pred['ARIMA'].values.reshape(-1,1)).flatten()
+
+#------------------------------------------------------------
+# MONTAGEM DA TABELA FINAL PARA CÁLCULOS DE PERFORMANCE
+# Adiciono ao DF de previsão a coluna de abertura e fechamento real, a fim
+# de verificar se a tendência de subida ou queda do índice
+# ou ação está sendo prevista corretamente, já que prevemos
+# o fechamento.
+#------------------------------------------------------------
+ab_fch = df.loc[df.index >= limite][['abertura', 'fechamento']].values
+df_pred[['abertura', 'fechamento']] = ab_fch
+df_pred = df_pred.rename(columns={'ARIMA': 'fechamento_predito'})
+df_pred['tendencia_real'] = resultado_target(df_pred['abertura'], df_pred['fechamento']) #diferença entre abertura e fechamento real
+df_pred['tendencia_predita'] = resultado_target(df_pred['abertura'], df_pred['fechamento_predito']) #diferença entre abertura e fechamento predito
+# cálculo dos índices de performance
+wmape_= wmape(df_pred['fechamento'].values, df_pred['fechamento_predito'].values)
+acuracia = accuracy_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values)
+f1 = f1_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values, average='macro')
+recall = recall_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values, average='macro')
+logger.info('Previsão e cálculos de performance realizados')
 
 
 #-----------------------------------
 # CONFIGURANDO AS ABAS
 #-----------------------------------
-tab1, tab2, tab3 = st.tabs(['Visualização', 'Previsão', 'Info'])
+tab1, tab2, tab3 = st.tabs(['Visualização', 'Detalhamento Previsão', 'Info'])
 #-----------------------------------
 #ABA DE VISUALIZAÇÃO DOS DADOS
 #-----------------------------------
+df_vis_pred = df_pred[['ds', 'fechamento_predito']].copy()
+df_vis_pred = df_vis_pred.set_index('ds')
+df_vis_pred = df_vis_pred.rename(columns={'fechamento_predito': 'fechamento'})
+
+primeiro_dia = df_vis_pred.loc[df_vis_pred.index == df_vis_pred.index[0]]
+df_vis_graf = df_vis[['fechamento']].copy()
+df_vis_graf.loc[primeiro_dia.index[0]] = primeiro_dia['fechamento'].values
+
 with tab1:
     fig1 = go.Figure()
 
     fig1.add_trace(go.Scatter(
-        x=df_vis.index,  # x-axis
-        y=df_vis['fechamento'],  # y-axis
+        x=df_vis_graf.index,  # x-axis
+        y=df_vis_graf['fechamento'],  # y-axis
         mode='lines',  # Connect data points with lines
         name='Fechamento do dia'  # Name in the legend
+    ))
+
+    fig1.add_trace(go.Scatter(
+        x=df_vis_pred.index,  # x-axis
+        y=df_vis_pred['fechamento'],  # y-axis
+        mode='lines',  # Connect data points with lines
+        name='Previsão',  # Name in the legend
+        line_color='rgba(255, 182, 193, .9)'
     ))
 
     # Layout parameters
@@ -196,65 +282,6 @@ with tab1:
 with tab2:
     st.write('Previsão para o período selecionado')
     
-    #--------------------------------------------
-    # ajuste do DF para uso dos modelos
-    #--------------------------------------------
-    
-    h=add_previsao #horizonte de previsão
-    limite = df.index[-h] #timestamp que determina treino e teste
-    #especificações do statsforecast
-    df_stats = df[['minima', 'maxima', 'fechamento']].copy()
-    df_ar = df_stats.reset_index()
-    df_ar = df_ar.rename(columns={'Date': 'ds', 'fechamento': 'y'})
-    df_ar['unique_id'] = 1
-    #definição de treino e teste
-    treino = df_ar.loc[df_ar['ds'] < limite]
-    teste = df_ar.loc[df_ar['ds'] >= limite]
-    logger.info('Períodos de treino e teste definidos')
- 
-    #------------------------------------------
-    # PREDIÇÃO DAS EXÓGENAS:
-    # Carrega os modelos já treinados, e depois junta as previsões
-    # em um único DF que irá servir de dados externos para o modelo
-    # principal.
-    #------------------------------------------
-        
-    model_min = joblib.load(f'app/models/model_prev_min_{PAPEL}.pkl')
-    logger.info('Modelo de previsão da mínima carregado')
-    exog_min = model_min.predict(h=h)
-    exog_min.rename(columns={'AutoARIMA': 'minima'}, inplace=True)
-    model_max = joblib.load(f'app/models/model_prev_max_{PAPEL}.pkl')
-    logger.info('Modelo de previsão da máxima carregado')
-    exog_max = model_max.predict(h=h)
-    exog_max.rename(columns={'AutoARIMA': 'maxima'}, inplace=True)
-    exog = exog_min.merge(exog_max[['ds', 'maxima']], on='ds', how='left')
-    logger.info('DF de máxima e mínima criado')
-    
-    #-------------------------------------------------------------
-    # LOAD DO MODELO TREINADO E PREDIÇÃO DO PERÍODO SELECIONADO
-    #-------------------------------------------------------------
-    modelo_ar = joblib.load(f'app/models/model_{PAPEL}.pkl')
-    logger.info('Modelo de previsão principal carregado')
-    df_pred = modelo_ar.predict(h=h, X_df=exog)
-    
-    #------------------------------------------------------------
-    # MONTAGEM DA TABELA FINAL PARA CÁLCULOS DE PERFORMANCE
-    # Adiciono ao DF de previsão a coluna de abertura e fechamento real, a fim
-    # de verificar se a tendência de subida ou queda do índice
-    # ou ação está sendo prevista corretamente, já que prevemos
-    # o fechamento.
-    #------------------------------------------------------------
-    ab_fch = df.loc[df.index >= limite][['abertura', 'fechamento']].values
-    df_pred[['abertura', 'fechamento']] = ab_fch
-    df_pred = df_pred.rename(columns={'AutoARIMA': 'fechamento_predito'})
-    df_pred['tendencia_real'] = resultado_target(df_pred['abertura'], df_pred['fechamento']) #diferença entre abertura e fechamento real
-    df_pred['tendencia_predita'] = resultado_target(df_pred['abertura'], df_pred['fechamento_predito']) #diferença entre abertura e fechamento predito
-    # cálculo dos índices de performance
-    wmape_= wmape(df_pred['fechamento'].values, df_pred['fechamento_predito'].values)
-    acuracia = accuracy_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values)
-    f1 = f1_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values, average='macro')
-    recall = recall_score(df_pred['tendencia_real'].values, df_pred['tendencia_predita'].values, average='macro')
-    logger.info('Previsão e cálculos de performance realizados')
     #--------------------------------------------------------
     # VISUALIZAÇÃO DOS DADOS PREVISTOS
     #--------------------------------------------------------
@@ -279,15 +306,16 @@ with tab2:
         x=df_pred.index,  # x-axis
         y=df_pred['fechamento_predito'],  # y-axis
         mode='lines+markers',  # Connect data points with lines
-        name='Fechamento Predito'  # Name in the legend
+        name='Fechamento Predito',  # Name in the legend
+        marker_color='rgba(255, 182, 193, .9)'
     ))
     
     fig2.add_trace(go.Scatter(
         x=df_pred.index,  # x-axis
         y=df_pred['abertura'],  # y-axis
         mode='lines+markers',  # Connect data points with lines
-        name='Abertura', # Name in the legend
-        marker_color='rgba(255, 182, 193, .9)'  
+        name='Abertura', # Name in the legend  
+        marker_color='rgba(0, 182, 193, .9)'
     ))
 
     # Layout parameters
@@ -302,18 +330,18 @@ with tab2:
     # CONFIGURAR UMA TABELA PARA VISUALIZAÇÃO DAS PREVISÕES
     #-----------------------------------------------------------------
     df_vis_tendencia = df_pred[['ds', 'tendencia_real', 'tendencia_predita']].copy()
-    df_vis_tendencia = df_vis_tendencia.rename(columns={'ds': 'Data'})
-    df_vis_tendencia['tendencia_real'] = df_vis_tendencia['tendencia_real'].apply(lambda x: 'Subiu' if x == 2 else('Igual' if x == 1 else 'Desceu'))
-    df_vis_tendencia['tendencia_predita'] = df_vis_tendencia['tendencia_predita'].apply(lambda x: 'Subiu' if x == 2 else('Igual' if x == 1 else 'Desceu'))
+    df_vis_tendencia = df_vis_tendencia.rename(columns={'ds': 'Data',   'tendencia_real': 'Tendência Real', 'tendencia_predita': 'Tendência Predita'})
+    df_vis_tendencia['Tendência Real'] = df_vis_tendencia['Tendência Real'].apply(lambda x: 'Subiu' if x == 2 else('Igual' if x == 1 else 'Desceu'))
+    df_vis_tendencia['Tendência Predita'] = df_vis_tendencia['Tendência Predita'].apply(lambda x: 'Subiu' if x == 2 else('Igual' if x == 1 else 'Desceu'))
     # FUNÇÃO QUE APLICA O ESTILO NAS CÉLULAS
     def estilo_previsao(row):
-        if row["tendencia_predita"] == row["tendencia_real"]:
+        if row["Tendência Predita"] == row["Tendência Real"]:
             cor = "background-color: #d4edda; color: #155724"
         else:
             cor = "background-color: #f8d7da; color: #721c24"
 
         return [
-            cor if col == "tendencia_predita" else ""
+            cor if col == "Tendência Predita" else ""
             for col in row.index
         ]
     
@@ -331,7 +359,7 @@ with tab2:
             st.dataframe(df_estilizada)
             
     with col1_2:
-        if df_vis_tendencia['Data'].count() > 6:
+        if df_vis_tendencia['Data'].count() > 7:
             df_estilizada = df_vis_tendencia.loc[7:].style.apply(estilo_previsao, axis=1)
             st.dataframe(df_estilizada)
         else:
